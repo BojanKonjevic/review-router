821247877780227eff59f0945a38e7fda3543d7f
Bojan <125093482+BojanKonjevic@users.noreply.github.com>
2026-09-15 21:30:27 +0200

    chore: project scaffold (#1)
    * chore: pnpm init
    
    * chore: add initial scripts and deps
    
    * chore: add tsconfig and clean up package.json
    
    * feat: write starter index.ts
    
    * style: explicit types on bootstrap
    
    * feat: add dotenv and .env
    
    * chore: import FastifyInstance as type only

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import Fastify, { type FastifyInstance } from "fastify";
+
+const port: number = Number(process.env.PORT ?? 3000);
+const app: FastifyInstance = Fastify({ logger: true });
+
+try {
+  await app.listen({ port });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
