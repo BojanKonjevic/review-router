7f4a61a766bb7ba58a48a80f1dc922eaa1b1c332
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-09-16 13:48:02 +0200

    feat: add parser so body is a buffer

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,23 @@ import Fastify, {
   type FastifyRequest,
   type FastifyReply,
 } from "fastify";
+import { createHmac } from "node:crypto";
 
 const port: number = Number(process.env.PORT ?? 3000);
 const app: FastifyInstance = Fastify({ logger: true });
+app.addContentTypeParser(
+  "application/json",
+  { parseAs: "buffer" },
+  function (req, body, done) {
+    done(null, body);
+  },
+);
 
 app.post("/webhooks/github", (req: FastifyRequest, res: FastifyReply) => {
+  const event = req.headers["x-github-event"];
+  const delivery = req.headers["x-github-delivery"];
+  const body = req.body;
+  app.log.info({ event, delivery, body });
   return res.status(200).send({ ok: true });
 });
 
