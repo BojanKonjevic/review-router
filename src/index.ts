675063eeaf55e2b789923326134a3851e7a6a9ca
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-09-16 13:23:13 +0200

    feat: bare test route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
-import Fastify, { type FastifyInstance } from "fastify";
+import Fastify, {
+  type FastifyInstance,
+  type FastifyRequest,
+  type FastifyReply,
+} from "fastify";
 
 const port: number = Number(process.env.PORT ?? 3000);
 const app: FastifyInstance = Fastify({ logger: true });
+
+app.post("/webhooks/github", (req: FastifyRequest, res: FastifyReply) => {
+  return res.status(200).send({ ok: true });
+});
 
 try {
   await app.listen({ port });
