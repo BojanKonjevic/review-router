4dbde6c5c2df7d49e14e3e480426644684134d04
Bojan Konjevic <konjevicbojan1@gmail.com>
2026-09-19 17:51:43 +0200

    feat: insert job in processor

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,20 @@
 import { type PrEventJob, redis } from "./queue.js";
-import { Job, Worker } from "bullmq";
+import { type Job, Worker } from "bullmq";
+import { db } from "./db/index.js";
+import { deliveries } from "./schema.js";
 
 async function processor(job: Job<PrEventJob>) {
-  console.log(job.data);
-  return;
+  const inserted = await db
+    .insert(deliveries)
+    .values({
+      id: job.data.deliveryId,
+      payload: JSON.stringify(job.data),
+    })
+    .onConflictDoNothing();
+  if (inserted.count === 0) {
+    console.log(job.data);
+    return;
+  }
 }
 
 const worker = new Worker("pr-events", processor, { connection: redis });
